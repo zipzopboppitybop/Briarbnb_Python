96f53084b60f5c01b824f49c4207e30579f09387
Brian <bw49078@gmail.com>
2024-03-15 16:19:23 -0500

    can edit booking from backend

--- app/api/bookings_routes.py
+++ app/api/bookings_routes.py
@@ -6,6 +6,33 @@ from app.forms import SpotForm, BookingForm
 
 booking_routes = Blueprint('bookings', __name__)
 
+@booking_routes.route('/<id>/edit', methods=['PUT'])
+@login_required
+def edit_booking(id):
+    """
+    Edit a booking and return it in a booking dictionary
+    """
+    user = current_user.to_dict()
+    form = BookingForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        booking = Booking.query.get(id)
+
+        if booking is None:
+            return {'errors': 'Booking does not exist'}, 404
+        
+        if user['id'] != booking.owner_id:
+            return {'errors': 'You do not have permission to edit this booking!'}, 403
+
+        booking.start_date = form.data['start_date']
+        booking.end_date = form.data['end_date']
+        db.session.commit()
+        return booking.to_dict()
+
+    if form.errors:
+        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
 @booking_routes.route('/<id>/delete', methods=['DELETE'])
 @login_required
 def delete_booking(id):
