282033b75f6ce6f1bc6c00c82ed54764eb6bbb33
Brian <bw49078@gmail.com>
2024-03-14 23:11:11 -0500

    user can now edit spot they own from backend

--- app/api/spots_routes.py
+++ app/api/spots_routes.py
@@ -58,6 +58,41 @@ def create_spot():
     if form.errors:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
     
+
+@spot_routes.route('/<id>/edit', methods=['PUT'])
+@login_required
+def edit_spot(id):
+    """
+    Edit a spot and return it in a spot dictionary
+    """
+    user = current_user.to_dict()
+    form = SpotForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        spot = Spot.query.get(id)
+
+        if spot is None:
+            return {'errors': 'Spot does not exist'}, 404
+        
+        if user['id'] != spot.owner_id:
+            return {'errors': 'You do not have permission to edit this spot!'}, 403
+
+        spot.name = form.data['name']
+        spot.address = form.data['address']
+        spot.city = form.data['city']
+        spot.state = form.data['state']
+        spot.country = form.data['country']
+        spot.lat = form.data['lat']
+        spot.lng = form.data['lng']
+        spot.description = form.data['description']
+        spot.price = form.data['price']
+        db.session.commit()
+        return spot.to_dict()
+
+    if form.errors:
+        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+    
 @spot_routes.route('/<id>/delete', methods=['DELETE'])
 @login_required
 def delete_spot(id):
