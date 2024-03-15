d61e717d49a02a37ef7e78de7784b098410754ef
Brian <bw49078@gmail.com>
2024-03-15 00:36:55 -0500

    can delete reviewfrom backend

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -5,3 +5,22 @@ from app.models import Review, User, db, Spot
 from app.forms import SpotForm
 
 review_routes = Blueprint('reviews', __name__)
+
+@review_routes.route('/<id>/delete', methods=['DELETE'])
+@login_required
+def delete_review(id):
+    """
+    Delete a Review
+    """
+    user = current_user.to_dict()
+    review = Review.query.get(id)
+
+    if review is None:
+        return {'errors': 'Review does not exist'}, 404
+
+    if user['id'] != review.owner_id:
+        return {'errors': 'You do not have permission to delete this Review!'}, 403
+
+    db.session.delete(review)
+    db.session.commit()
+    return {'message': 'Review deleted successfully'}
